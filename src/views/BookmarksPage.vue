2dbf33367ae14945d2a4a508561bbb847ba920dd
nevyk <73322863+nevyk@users.noreply.github.com>
2022-03-12 00:24:34 -0500

    feat(bookmarks): added tags menu

--- src/views/BookmarksPage.vue
+++ src/views/BookmarksPage.vue
@@ -3,15 +3,29 @@ import TheToolbar from '../components/TheToolbar.vue';
 import {
   IonPage,
   IonHeader,
-  IonContent,
   IonItem,
   IonList,
   IonLabel,
   IonButton,
-  IonIcon
+  IonIcon,
+  IonContent,
+  IonSplitPane,
+  IonMenu,
+  IonTitle,
+  IonToolbar
 } from '@ionic/vue';
-import { pencilOutline, trashOutline } from 'ionicons/icons';
-import { reactive } from 'vue';
+import { pencilOutline, trashOutline, pricetagsOutline } from 'ionicons/icons';
+import { ref, reactive, computed } from 'vue';
+
+const tags = computed(() => {
+  const prefix = 'tag';
+  let tagArray = [];
+
+  for (let t = 1; t < 30; t++) {
+    tagArray.push(`${prefix}${t}`);
+  }
+  return tagArray;
+});
 
 const data = reactive([
   {
@@ -35,26 +49,45 @@ function handleEdit() {
 
 <template>
   <ion-page>
-    <ion-header><TheToolbar></TheToolbar></ion-header>
-    <ion-content class="ion-padding">
-      <ion-list>
-        <ion-item
-          v-for="bookmark in data"
-          :key="bookmark.id"
-          :href="bookmark.url"
-          target="_blank"
-          rel="noopener"
-        >
-          <ion-label>{{ bookmark.name }}</ion-label>
-          <ion-button fill="clear" color="primary" @click.prevent="handleEdit">
-            <ion-icon slot="icon-only" :icon="pencilOutline"></ion-icon>
-          </ion-button>
-          <ion-button fill="clear" color="danger" @click.prevent="handleEdit">
-            <ion-icon slot="icon-only" :icon="trashOutline"></ion-icon>
-          </ion-button>
-        </ion-item>
-      </ion-list>
-    </ion-content>
+    <ion-split-pane content-id="main-content">
+      <!--  the side menu  -->
+      <ion-menu content-id="main">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Tags</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content>
+          <ion-list lines="none">
+            <ion-item v-for="tag in tags" :key="tag" :detail="false" button>
+              <ion-label>{{ tag }}</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </ion-menu>
+
+      <ion-content id="main-content">
+        <ion-header><TheToolbar></TheToolbar></ion-header>
+        <!-- Bookmark List -->
+        <ion-list>
+          <ion-item
+            v-for="bookmark in data"
+            :key="bookmark.id"
+            :href="bookmark.url"
+            target="_blank"
+            rel="noopener"
+          >
+            <ion-label>{{ bookmark.name }}</ion-label>
+            <ion-button fill="clear" color="primary" @click.prevent="handleEdit">
+              <ion-icon slot="icon-only" :icon="pencilOutline"></ion-icon>
+            </ion-button>
+            <ion-button fill="clear" color="danger" @click.prevent="handleEdit">
+              <ion-icon slot="icon-only" :icon="trashOutline"></ion-icon>
+            </ion-button>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-split-pane>
   </ion-page>
 </template>
 
