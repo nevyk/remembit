f75286bb63c02ba4b3706503cc32a6a3f65a0cf6
nevyk <73322863+nevyk@users.noreply.github.com>
2022-07-04 11:31:28 -0400

    Chore/add capacitor and ionic (#25)
    * chore: add ionic and capacitor to project
    
    * ci(main): add branch filter for push trigger

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.rmembit',
+  appName: 'remembit',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
